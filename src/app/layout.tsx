956e5bd004330ecc4e7500084fd9fea56d91cf19
beawkiattisak <bbeawz.2550@gmail.com>
2023-10-19 09:01:14 +0700

    layout.tsx fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'beaw.bio',
-  description: 'beaw.bio'
-}
+  description: 'Kiattisak Sanburee Portfolio Website',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-
       <head>
-      <link rel="icon" href="/favicon.ico" />
-      <meta />
-      <meta property="og:image" content="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content="https://media.discordapp.net/attachments/1164170305158258728/1164381637291823184/Group_2.png?ex=654301e7&is=65308ce7&hm=652586cd2771b503e06fe71fa08bdbcdc98dbfe71fa08bdbcdc98dbfe71fa08bdbcdc98d777f&=&width=2160&height=952" />
+        <meta property="og:title" content="beaw.bio" />
+        <meta property="og:description" content="Kiattisak Sanburee Portfolio Website" />
       </head>
-
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
