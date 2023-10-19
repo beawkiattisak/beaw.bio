ae6607679004c716b0233492386291c959052833
beawkiattisak <bbeawz.2550@gmail.com>
2023-10-19 09:08:26 +0700

    Fix Image meta size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:image" content="https://media.discordapp.net/attachments/1164170305158258728/1164381637291823184/Group_2.png?ex=654301e7&is=65308ce7&hm=652586cd2771b503e06fe71fa08bdbcdc98dbfe71fa08bdbcdc98dbfe71fa08bdbcdc98d777f&=&width=2160&height=952" />
+        <meta property="og:image" content="https://media.discordapp.net/attachments/1164170305158258728/1164384105677475951/asdasdadad.png?ex=65430434&is=65308f34&hm=7c513d3bcd5ef65bab8cc033aa5e4fed75ac9224456b3c862b7d3e4803580d41&=&width=2160&height=952" />
         <meta property="og:title" content="beaw.bio" />
         <meta property="og:description" content="Kiattisak Sanburee Portfolio Website" />
       </head>
