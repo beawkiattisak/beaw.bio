3a43a1b2ab817f25670f5dad325cc5dbca1d54db
beawkiattisak <bbeawz.2550@gmail.com>
2023-11-05 16:10:33 +0700

    Change website title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'beaw.bio',
+  title: 'Kiattisak (Beaw) Sanburee | Portfolio Website',
   description: 'Kiattisak Sanburee Portfolio Website',
 };
 
